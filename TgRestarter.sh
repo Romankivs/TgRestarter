5b7d265e1d15d1179552399fb43e21682ab27f76
Romankivs <45734354+Romankivs@users.noreply.github.com>
2021-03-24 12:37:26 +0200

    Add files via upload

--- TgRestarter.sh
+++ TgRestarter.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
 #constant variables
-readonly MEMORY_LIM_IN_MEGABYTES=1000
-
-TELEGRAM_PID=$(pidof telegram-desktop)
-while [ -z "$TELEGRAM_PID" ]
-do
-    sleep 1m
-    TELEGRAM_PID=$(pidof telegram-desktop) 
-done
-TELEGRAM_WINDOW_ID=$(xdotool search --sync --onlyvisible --pid ${TELEGRAM_PID})
+readonly MEMORY_LIM_IN_MEGABYTES=824
 
 while :
 do
+    TELEGRAM_PID=$(pidof telegram-desktop)
+    #wait until telegram will be opened
+    while [ -z "$TELEGRAM_PID" ]
+    do
+        sleep 1m
+        TELEGRAM_PID=$(pidof telegram-desktop) 
+    done
+    TELEGRAM_WINDOW_ID=$(xdotool search --sync --onlyvisible --pid ${TELEGRAM_PID})
     ACTIVE_WINDOW_ID=$(xdotool getactivewindow)
-    let MEMORY_USED_BY_TELEGRAM_DESKTOP_IN_MEGABYTES=$(ps -q ${TELEGRAM_PID} -o rss=)/1024
-    if [ "$TELEGRAM_WINDOW_ID" -ne "$ACTIVE_WINDOW_ID" ] && [ "$MEMORY_USED_BY_TELEGRAM_DESKTOP_IN_MEGABYTES" -ge "${MEMORY_LIM_IN_MEGABYTES}" ]
+    let MEMORY_USED_BY_TELEGRAM_IN_MEGABYTES=$(ps -q ${TELEGRAM_PID} -o rss=)/1024
+    if [ "$TELEGRAM_WINDOW_ID" -ne "$ACTIVE_WINDOW_ID" ] && [ "$MEMORY_USED_BY_TELEGRAM_IN_MEGABYTES" -ge "${MEMORY_LIM_IN_MEGABYTES}" ]
     then
         #restart telegram-desktop
         kill $(pidof telegram-desktop)
@@ -26,5 +26,5 @@ do
         #minimize window 
         xdotool windowminimize $TELEGRAM_WINDOW_ID
     fi
-    sleep 1
+    sleep 1m
 done
